37bb2164087f84c2ae62dca4e34a2c6f3b1e46ee
sibell06 <sibel.hodzhova@abv.bg>
2026-07-31 17:23:51 +0300

    chore(loyalty-svc): scaffold standalone microservice project

---
+++ loyalty-svc/src/main/java/com/softuni/loyaltysvc/LoyaltySvcApplication.java
@@ -0,0 +1,12 @@
+package com.softuni.loyaltysvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltySvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyaltySvcApplication.class, args);
+    }
+}
