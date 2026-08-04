812c5e545e50db1c0a8e22b50dea273304f645d6
sibell06 <sibel.hodzhova@abv.bg>
2026-08-04 17:37:18 +0300

    feat: add openfeign dependency and enable feign clients

--- src/main/java/com/softuni/stayeasy/StayEasyApplication.java
+++ src/main/java/com/softuni/stayeasy/StayEasyApplication.java
@@ -2,8 +2,10 @@ package com.softuni.stayeasy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class StayEasyApplication {
 
     public static void main(String[] args) {
